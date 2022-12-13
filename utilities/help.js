69c8ff7eba136a28e48988359cadcf9b286d6324
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2022-12-13 15:12:51 -0800

    subtle change

--- utilities/help.js
+++ utilities/help.js
@@ -1,7 +1,7 @@
 module.exports = () => console.table([
     {command: "gmd wp", action: "Create a Wordpress site"},
     {command: "gmd wp --sage", action: "Create a Wordpress site with Sage theme"},
-    {command: "gmd wp headless", action: "Create a headless Wordpress site"},
+    {command: "gmd wp --headless", action: "Create a headless Wordpress site"},
     {command: "gmd wp --multisite", action: "Create a Wordpress multi-site with Sage theme"},
     {command: "gmd wp --multisite --sage", action: "Create a Wordpress multi-site with Sage theme"},
     {command: "gmd gatsby --headless", action: "Create a headless Gatsby site"},
